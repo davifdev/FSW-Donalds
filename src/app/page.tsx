be959820421b77def33987aa6139fd86c3fc6ec5
Davi Fernandes <davi.fer159@gmail.com>
2026-07-06 13:33:38 -0300

    chore: add prerttier and eslint setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <h1 className="text-red-500">Hello World</h1>;
+  return (
+    <>
+      <h1 className="p-5 text-red-500">Hello World</h1>
+      <h1 className="mx-0 mt-1 bg-white p-5 text-red-500">Hello World</h1>
+    </>
+  );
 };
 
 export default HomePage;
